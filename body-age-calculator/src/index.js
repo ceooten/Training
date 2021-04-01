87451dabd6d9345bc80555daaa6c50b4ac6a166f
Caleb Ooten <sdtrojans55@gmail.com>
2021-04-01 14:51:36 -0400

    Update index.js
    Commented and removed the chrome extension Redux DevTools functionality that caused an error if DevTools was not installed.

--- body-age-calculator/src/index.js
+++ body-age-calculator/src/index.js
@@ -18,10 +18,8 @@ const rootReducer = combineReducers({
   results: resultsReducer
 });
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk)
-));
+//const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
